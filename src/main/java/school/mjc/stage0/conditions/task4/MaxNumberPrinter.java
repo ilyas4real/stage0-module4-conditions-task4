7daba84b569f4203c31ba16165ec68dfccc89890
ilyas4real <iliyas.ysmaiyl@gmail.com>
2023-02-04 18:40:54 +0100

    completed

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int largest, temp;
+        temp = first > second ? first:second;
+        largest = third > temp ? third:temp;
+        System.out.println(largest);
     }
 }
